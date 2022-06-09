14a28dc92bdad1579c9b2ccd188c9cd8f34121f4
gokhanguz <67508424+gokhanguz@users.noreply.github.com>
2022-06-10 01:35:39 +0200

    Add controller

--- src/main/java/com/cydeo/controller/ApiController.java
+++ src/main/java/com/cydeo/controller/ApiController.java
@@ -2,7 +2,10 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.ParentService;
+import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +15,15 @@ import java.util.List;
 @RestController
 public class ApiController {
     private final TeacherService teacherService;
+    private final StudentService studentService;
+    private final ParentService parentService;
 
-    public ApiController(TeacherService teacherService) {
+    public ApiController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
         this.teacherService = teacherService;
+        this.studentService = studentService;
+        this.parentService = parentService;
     }
+
 
     @GetMapping("/teachers")
     public List<TeacherDTO> readAllTeachers(){
@@ -26,8 +34,17 @@ public class ApiController {
     @GetMapping("/students")
     public ResponseEntity<ResponseWrapper> readAllStudents(){
 
-        return ResponseEntity.ok(new ResponseWrapper());
+        return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved",studentService.findAll()));
+    }
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+
+        ResponseWrapper responseWrapper=
+                new ResponseWrapper(true,"Parent are successfully retrived", HttpStatus.OK.value(), parentService.findAll());
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseWrapper);
     }
 
 
-    }
+}
